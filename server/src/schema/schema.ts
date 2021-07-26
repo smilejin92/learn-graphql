a5b49ac2f64092f10c90891582ac61acc203a42b
smilejin92 <smilejin92@gmail.com>
2021-07-26 10:58:24 +0900

    [feat] Author Type 추가

--- server/src/schema/schema.ts
+++ server/src/schema/schema.ts
@@ -1,5 +1,6 @@
 import {
   GraphQLID,
+  GraphQLInt,
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
@@ -11,6 +12,12 @@ let books = [
   { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
   { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
+
+let authors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Brandon Sanderson', age: 42, id: '2' },
+  { name: 'Terry Pratchett', age: 66, id: '3' },
 ];
 
 // 1. define type
@@ -25,6 +32,15 @@ const BookType = new GraphQLObjectType({
   }),
 });
 
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }),
+});
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -36,6 +52,15 @@ const RootQuery = new GraphQLObjectType({
       resolve: (parent, args) => {
         // code to get data from DB or other source
         return _.find(books, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve: (parent, args) => {
+        return _.find(authors, { id: args.id });
       },
     },
   },
