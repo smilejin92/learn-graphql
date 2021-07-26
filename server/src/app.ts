2b50e2c5b2eec10dab8721ab685f0d7597742b5b
smilejin92 <smilejin92@gmail.com>
2021-07-26 14:32:10 +0900

    [feat] Connect MongoDB when launched

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,20 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
+import { MongoClient } from 'mongodb';
 import schema from './schema/schema';
 
 const app = express();
+
+const { USER_ID, USER_PWD, DB_URL } = process.env;
+const uri = `mongodb+srv://${USER_ID}:${USER_PWD}@${DB_URL}`;
+const client = new MongoClient(uri);
+
+client.connect(err => {
+  // const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log('connected to database');
+  client.close();
+});
 
 // graphql middleware
 app.use(
