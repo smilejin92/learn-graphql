f5eba00dd4802c18ab1a6ea263df33894d4e59e8
smilejin92 <smilejin92@gmail.com>
2021-07-26 10:49:28 +0900

    [feat] server 작성

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,18 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './schema/schema';
 
 const app = express();
+
+// graphql middleware
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    // define schema - allow queries to jump into graph and retrieve data
+    schema,
+    graphiql: true,
+  }),
+);
 
 app.listen(4000, () => {
   console.log('listening for requests on port 4000');
