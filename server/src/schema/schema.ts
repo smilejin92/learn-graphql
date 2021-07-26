f56aaf8fde8fdbc57aa2b493db03843299e2ff89
smilejin92 <smilejin92@gmail.com>
2021-07-26 16:30:33 +0900

    [feat] Update resolve functions

--- server/src/schema/schema.ts
+++ server/src/schema/schema.ts
@@ -24,7 +24,7 @@ const BookType: GraphQLObjectType = new GraphQLObjectType({
       type: AuthorType,
       resolve: (parent, args) => {
         // parent = book(id)
-        // return _.find(authors, { id: parent.authorId });
+        return Author.findById(parent.authorId);
       },
     },
   }),
@@ -40,7 +40,9 @@ const AuthorType: GraphQLObjectType = new GraphQLObjectType({
       type: new GraphQLList(BookType),
       resolve: (parent, args) => {
         // parent = AuthorType
-        // return _.filter(books, { authorId: parent.id });
+        return Book.find({
+          authorId: parent.id,
+        });
       },
     },
   }),
@@ -56,7 +58,7 @@ const RootQuery = new GraphQLObjectType({
       },
       resolve: (parent, args) => {
         // code to get data from DB or other source
-        // return _.find(books, { id: args.id });
+        return Book.findById(args.id);
       },
     },
     author: {
@@ -65,19 +67,20 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLID },
       },
       resolve: (parent, args) => {
-        // return _.find(authors, { id: args.id });
+        return Author.findById(args.id);
       },
     },
     books: {
       type: new GraphQLList(BookType),
       resolve: (parent, args) => {
-        // return books;
+        // find all books
+        return Book.find({});
       },
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve: (parent, args) => {
-        // return authors;
+        return Author.find({});
       },
     },
   },
