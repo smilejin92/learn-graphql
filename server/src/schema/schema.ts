e7f9f32547d5b64c6417d3b31b9ff80f78c323dd
smilejin92 <smilejin92@gmail.com>
2021-07-26 11:23:50 +0900

    [feat] Author -> Book 관계 생성

--- server/src/schema/schema.ts
+++ server/src/schema/schema.ts
@@ -4,6 +4,7 @@ import {
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
+  GraphQLList,
 } from 'graphql';
 import _ from 'lodash';
 
@@ -13,6 +14,9 @@ let books = [
   { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
   { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+  { name: 'The Color of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
 ];
 
 let authors = [
@@ -24,8 +28,9 @@ let authors = [
 // 1. define type
 // 2. define relationship between types
 // 3. define root queries
-const BookType = new GraphQLObjectType({
+const BookType: GraphQLObjectType = new GraphQLObjectType({
   name: 'Book',
+  // 함수를 사용하는 이유 = 함수 스코프 내부에서 참조하는 타입이 런타임에 먼저 생성될 수 있도록
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
@@ -40,12 +45,19 @@ const BookType = new GraphQLObjectType({
   }),
 });
 
-const AuthorType = new GraphQLObjectType({
+const AuthorType: GraphQLObjectType = new GraphQLObjectType({
   name: 'Author',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: (parent, args) => {
+        // parent = AuthorType
+        return _.filter(books, { authorId: parent.id });
+      },
+    },
   }),
 });
 
